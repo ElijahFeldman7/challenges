53cf783ae3695a272f901d6a12018fa5a16831ac
ElijahFeldman7 <elijah.feldman.sunshine.123@gmail.com>
2026-06-08 12:42:37 -0400

    postgres

--- terac/lib/prisma.ts
+++ terac/lib/prisma.ts
@@ -1,15 +1,18 @@
-import { PrismaClient } from "../generated/client/client";
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from "../generated/client/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || 'file:./dev.db' });
-  return new PrismaClient({ adapter });
-};
+  const adapter = new PrismaPg({ 
+    connectionString: process.env.DATABASE_URL! 
+  })
+  
+  return new PrismaClient({ adapter })
+}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient | undefined
+}
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
